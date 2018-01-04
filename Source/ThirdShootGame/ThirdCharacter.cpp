642cd1322036e44a78fab55868d1b386bc16657b
WZHQ <1208129890@qq.com>
2018-01-04 11:19:07 +0800

    初始提交

---
+++ Source/ThirdShootGame/ThirdCharacter.cpp
@@ -0,0 +1,62 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ThirdCharacter.h"
+
+
+// Sets default values
+AThirdCharacter::AThirdCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AThirdCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AThirdCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AThirdCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+//Implement CalculateHealth
+void AThirdCharacter::CalculateHealth(float Delta)
+{
+	Health += Delta;
+	OnCalculateDead();
+}
+//Implement CalculateDead
+void AThirdCharacter::OnCalculateDead()
+{
+	if (Health <= 0)
+	{
+		isDead = true;
+	}
+	else
+	{
+		isDead = false;
+	}
+}
+//Implement PostEditChangeProperty
+#if WITH_EDITOR
+void AThirdCharacter::PostEditChangeProperty(FPropertyChangedEvent &PropertyChangedEvent)
+{
+	isDead = false;
+	Health = 100;
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+		OnCalculateDead();
+}
+#endif
---
+++ Source/ThirdShootGame/ThirdCharacter.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ThirdCharacter.generated.h"
+
+UCLASS()
+class THIRDSHOOTGAME_API AThirdCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AThirdCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Base Character")
+		float Health = 100;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Base Character")
+		bool isDead = false;
+	virtual void OnCalculateDead();
+	UFUNCTION(BlueprintCallable, Category = "Base Character")
+		virtual void CalculateHealth(float delta);
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(FPropertyChangedEvent&PropertyChangedEvent)
+		override;
+#endif
+	
+};
